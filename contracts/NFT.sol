38adab3a6fc64cf096eafe977cde35aa554b4240
aumerez <aumerez@gmail.com>
2022-03-15 08:21:35 -0400

    New contract with Open sea deployment

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,34 +1,35 @@
-//import Open Zepplin contracts
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
 
-pragma solidity ^0.8.2;
-
-//import Open Zepplin contracts
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721 {
-    uint256 private _tokenIds;
-    
-    constructor() ERC721("Name", "Symbol") {}
-    
-//use the mint function to create an NFT
-    function mint()
-    public
-    returns (uint256)
-    {
-        _tokenIds += 1;
-        _mint(msg.sender, _tokenIds);
-        return _tokenIds;
-    }
-    
-//in the function below include the CID of the JSON folder on IPFS
-    function tokenURI(uint256 _tokenId) override public pure returns(string memory) {
-        return string(
-            abi.encodePacked(
-                "https://ipfs.io/ipfs/QmehAeNkXqCyy62P9AhxrhyiNWMuuofWHcW4jVaAMDQsSv/",
-                Strings.toString(_tokenId),
-                ".json"
-            )
-        );
-    }
+  using Counters for Counters.Counter;
+
+  Counters.Counter private currentTokenId;
+
+  /// @dev Base token URI used as a prefix by tokenURI().
+  string public baseTokenURI;
+
+  constructor() ERC721("NFTTutorial", "NFT") {
+    baseTokenURI = "";
+  }
+
+  function mintTo(address recipient) public returns (uint256) {
+    currentTokenId.increment();
+    uint256 newItemId = currentTokenId.current();
+    _safeMint(recipient, newItemId);
+    return newItemId;
+  }
+
+  /// @dev Returns an URI for a given token ID
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseTokenURI;
+  }
+
+  /// @dev Sets the base token URI prefix.
+  function setBaseTokenURI(string memory _baseTokenURI) public {
+    baseTokenURI = _baseTokenURI;
+  }
 }
